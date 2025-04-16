ab2e078a87329fe0f229e48e2530fdb4b732d63f
Taras <r.taras.t.1@gmail.com>
2025-04-16 22:27:39 +0300

    add components files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
   return (
     <>
-
+      <Description></Description>
+      <Options></Options>
+      <Feedback></Feedback>
     </>
   )
 }
