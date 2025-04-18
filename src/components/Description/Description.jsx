071de53bf59d4cff113ee2fbf1b15a50871c4b0a
Taras <r.taras.t.1@gmail.com>
2025-04-18 20:59:30 +0300

    upd styles

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,8 +1,10 @@
+import css from './Description.module.css';
+
 export default function Description() {
     return (
         <>
-            <h1><span>Sip</span> Happens Café</h1>
-            <p>Please leave your feedback <span>about</span> our service <span>by</span> selecting one <span>of the</span> options <span>below</span>.</p>
+            <h1><span className={css.greenSpan}>Sip</span> Happens Café</h1>
+            <p>Please leave your feedback <span className={css.violetSpan}>about</span> our service <span className={css.violetSpan}>by</span> selecting one <span className={css.violetSpan}>of the</span> options <span className={css.violetSpan}>below</span>.</p >
         </>
     )
 }
