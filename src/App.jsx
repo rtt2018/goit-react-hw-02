def03d63dd5fe353b2f883db35580666aa24da76
Taras <r.taras.t.1@gmail.com>
2025-04-18 17:49:47 +0300

    add save to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Description from './components/Description/Description.jsx'
 import Options from './components/Options/Options.jsx'
@@ -6,21 +6,37 @@ import Feedback from './components/Feedback/Feedback.jsx'
 import Notification from './components/Notification/Notification.jsx'
 
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(() => {
+    const restoreFeedback = window.localStorage.getItem("feedback");
+    if (restoreFeedback !== null) {
+      return JSON.parse(restoreFeedback).good;
+    }
+    return 0;
+  });
+
+  const [neutral, setNeutral] = useState(() => {
+    const restoreFeedback = window.localStorage.getItem("feedback");
+    if (restoreFeedback !== null) {
+      return JSON.parse(restoreFeedback).neutral;
+    }
+    return 0;
+  });
+
+  const [bad, setBad] = useState(() => {
+    const restoreFeedback = window.localStorage.getItem("feedback");
+    if (restoreFeedback !== null) {
+      return JSON.parse(restoreFeedback).bad;
+    }
+    return 0;
+  });
 
   const totalFeedback = good + neutral + bad;
-  console.log("🚀 ~ App ~ totalFeedback:", totalFeedback)
-  const procentPositive = Math.round((good / totalFeedback) * 100);
+  const procentPositive = isNaN(totalFeedback) ? 0 : Math.round((good / totalFeedback) * 100);
+  const feedbacks = { good, neutral, bad };
 
-  const feedbackAll = {
-    good,
-    neutral,
-    bad,
-    totalFeedback,
-    procentPositive
-  };
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify({ good, neutral, bad }));
+  }, [good, neutral, bad]);
 
   const updGood = () => {
     setGood(good + 1);
@@ -40,12 +56,11 @@ function App() {
     setBad(0);
   }
 
-
   return (
     <>
       <Description />
       <Options totalFB={totalFeedback} onGood={updGood} onNeutral={updNeutral} onBad={updBad} onReset={reSet} />
-      {totalFeedback > 0 && <Feedback value={feedbackAll} />}
+      {totalFeedback > 0 && <Feedback value={{ ...feedbacks, totalFeedback, procentPositive }} />}
       {totalFeedback === 0 && <Notification />}
     </>
   )
