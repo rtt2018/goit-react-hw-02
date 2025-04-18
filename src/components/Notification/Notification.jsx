b922a15a84eb943fa1255addbda8be9f14a5c285
Taras <r.taras.t.1@gmail.com>
2025-04-18 20:48:12 +0300

    upd

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,7 +3,7 @@ import css from './Notification.module.css';
 export default function Notification() {
     return (
         <div className={css.container}>
-            <p >No feedback given</p>
+            <p className={css.notification}>No feedback given</p>
         </div>
     );
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,10 +3,10 @@ import css from './Options.module.css';
 export default function Options({ totalFB, onGood, onNeutral, onBad, onReset }) {
     return (
         <div className={css.container}>
-            <button onClick={onGood}>Good</button>
-            <button onClick={onNeutral}>Neutral</button>
-            <button onClick={onBad}>Bad</button>
-            {totalFB > 0 && <button onClick={onReset}>Reset</button>}
+            <button type='button' aria-label='Good feedback button' onClick={onGood}>Good</button>
+            <button type='button' aria-label='Neutral feedback button' onClick={onNeutral}>Neutral</button>
+            <button type='button' aria-label='Bad feedback button' onClick={onBad}>Bad</button>
+            {totalFB > 0 && <button type='button' aria-label='Reset feedback button' onClick={onReset}>Reset</button>}
         </div>
     );
 
